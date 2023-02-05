ee984a73dcc97bcf97aeddcbd78c00bd9c708952
Mohit <mohit.patel1966@gmail.com>
2023-02-05 22:12:25 +0530

    coded from forked repo of fcc

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-const container = document.getElementById("text");
-console.log(container);
+// const container = document.getElementById("text");
+// console.log(container);
+
+const moment = require('moment');
+const simpleGit = require('simple-git')
+
+const date = moment().subtract(7,'d').format();
+
+simpleGit().add(['./main.js']).commit("coded from forked repo of fcc",{'--date': date}).push();
