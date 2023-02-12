78aafd1164f6460fc31b6a74496583efe439d222
Mohit <mohit.patel1966@gmail.com>
2023-02-12 22:14:51 +0530

    removed react app

--- main.js
+++ main.js
@@ -1,9 +1,2 @@
-// const container = document.getElementById("text");
-// console.log(container);
-
-const moment = require('moment');
-const simpleGit = require('simple-git')
-
-const date = moment().subtract(7,'d').format();
-
-simpleGit().add(['./main.js']).commit("coded from forked repo of fcc",{'--date': date}).push();
+const container = document.getElementById("text");
+console.log(container);
